5f423229076a844d6c15d162c36f6322b4bbf328
222TheMaster222 <222themaster222@proton.me>
2025-03-26 05:49:21 -0500

    add crafting program to contract

--- src/sage/appContext.ts
+++ src/sage/appContext.ts
@@ -1,15 +1,18 @@
 import { Connection, Keypair, PublicKey, Transaction, VersionedTransaction } from "@solana/web3.js";
 import { AnchorProvider, Program } from "@staratlas/anchor";
+import { CRAFTING_IDL, CraftingIDLProgram } from "@staratlas/crafting";
 import { PLAYER_PROFILE_IDL, PlayerProfileIDLProgram } from "@staratlas/player-profile";
 import { SAGE_IDL, SageIDLProgram } from "@staratlas/sage";
 
+const CRAFTING_PROGRAM_ID = new PublicKey('CRAFT2RPXPJWCEix4WpJST3E7NLf79GTqZUL75wngXo5');
 const SAGE_PROGRAM_ID = new PublicKey('SAGE2HAwep459SNq61LHvjxPk4pLPEJLoMETef7f7EE');
 const PLAYER_PROFILE_ID = new PublicKey('pprofELXjL5Kck7Jn5hCpwAL82DpTkSYBENzahVtbc9');
 
 // Shared application context
 export interface AppContext {
-    connection: Connection;
-    sage: SageIDLProgram;
+    connection: Connection
+    crafting: CraftingIDLProgram
+    sage: SageIDLProgram
     playerProfile: PlayerProfileIDLProgram
 }
 
@@ -26,6 +29,7 @@ export function createAppContext(connection: Connection): AppContext {
 
     return {
         connection,
+        crafting: new Program(CRAFTING_IDL, CRAFTING_PROGRAM_ID, provider),
         sage: new Program(SAGE_IDL, SAGE_PROGRAM_ID, provider),
         playerProfile: new Program(PLAYER_PROFILE_IDL, PLAYER_PROFILE_ID, provider),
     };
