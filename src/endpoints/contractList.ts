da0c857c8c2c45dca4b071a12591fe7c1863fda2
222TheMaster222 <222themaster222@proton.me>
2025-03-30 16:19:56 -0500

    map out contracts a bit

--- src/endpoints/contractList.ts
+++ src/endpoints/contractList.ts
@@ -4,8 +4,9 @@ import { Connection, Keypair, PublicKey } from "@solana/web3.js";
 import Papa from 'papaparse';
 import { Program, AnchorProvider, BN, setProvider, utils, workspace } from "@coral-xyz/anchor";
 import { Srsly, IDL } from "../srsly/idl/srsly_idl"
-import { createAppContext } from "sage";
-
+import { createAppContext, getFleets } from "sage";
+import { byteArrayToString } from "@staratlas/data-source";
+import { Faction } from "@staratlas/profile-faction";
 export class ContractList extends OpenAPIRoute {
 	schema = {
 		tags: ["Fleet Rental Contracts"],
@@ -25,6 +26,7 @@ export class ContractList extends OpenAPIRoute {
 	async handle(c: { env: Env }) {
 
 		const connection = new Connection(c.env.RPC_URL, { commitment: "confirmed" })
+		const context = createAppContext(connection);
 		const wallet = {
 			publicKey: Keypair.generate().publicKey,
 			signAllTransactions: async txs => txs,
@@ -38,24 +40,50 @@ export class ContractList extends OpenAPIRoute {
 
 		const program = new Program<Srsly>(IDL, provider)
 
-		const contracts = await program.account.contractState.all();
-		//const state = await program.account.rentalState.all();
+		const [contracts, rentals, fleets] = await Promise.all([
+			program.account.contractState.all(),
+			program.account.rentalState.all(),
+			getFleets(context),
+		]);
 
-		const data = contracts.map(c => ({
-			'Contract Address': c.publicKey.toBase58(),
-			'Rental State': c.account.currentRentalState.toBase58(),
-			'Min Duration': c.account.durationMin.toString(),
-			'Max Duration': c.account.durationMax.toString(),
-			'Fleet': c.account.fleet.toBase58(),
-			'Game ID': c.account.gameId.toBase58(),
-			'Owner': c.account.owner.toBase58(),
-			'Owner Profile': c.account.ownerProfile.toBase58(),
-			'Owner Token Account': c.account.ownerTokenAccount.toBase58(),
-			// 'Payment Frequency': JSON.stringify(c.account.paymentsFeq), // Uncomment if needed
-			'Rate': c.account.rate.toString(),
-			'Marked for Closure': c.account.toClose,
-			'Contract Version': c.account.version,
-		}))
+		const results = contracts.map(contract => {
+
+			const rental = contract.account.currentRentalState.equals(PublicKey.default)
+				? undefined
+				: rentals.find(r => r.publicKey.equals(contract.account.currentRentalState))
+
+			const fleet = fleets.find(f => f.key.equals(contract.account.fleet))
+
+			return {
+				contract,
+				rental,
+				fleet,
+			}
+		})
+
+		const data = results.map(result => {
+			const { contract: c, fleet: f, rental: r } = result;
+
+
+			return {
+				'Contract Address': c.publicKey.toBase58(),
+				'Rental State': c.account.currentRentalState.toBase58(),
+				'Min Duration': c.account.durationMin.toString(),
+				'Max Duration': c.account.durationMax.toString(),
+				'Fleet': c.account.fleet.toBase58(),
+				'Game ID': c.account.gameId.toBase58(),
+				'Owner': c.account.owner.toBase58(),
+				'Owner Profile': c.account.ownerProfile.toBase58(),
+				'Owner Token Account': c.account.ownerTokenAccount.toBase58(),
+				// 'Payment Frequency': JSON.stringify(c.account.paymentsFeq), // Uncomment if needed
+				'Contract Rate': c.account.rate.toString(),
+				'Marked for Closure': c.account.toClose,
+				'Contract Version': c.account.version,
+				'Faction': Faction[f.data.faction],
+				'Fleet Label': byteArrayToString(f.data.fleetLabel),
+				'Rental Rate': r?.account.rate.toString(),
+			}
+		})
 
 
 		const csv = Papa.unparse(data)
